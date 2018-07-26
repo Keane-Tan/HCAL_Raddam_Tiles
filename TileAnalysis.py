243b505f7c1643e6a6806d2464328caf39453f6b
Aran <aran.garcia-bellido@cern.ch>
2018-07-26 08:55:31 +0200

    Changed axis for fit

--- TileAnalysis.py
+++ TileAnalysis.py
@@ -19,12 +19,12 @@ s3=Data[Data['Dose (Mrad)']==1.7] # the full table only for sample3 tiles (with 
 
 beamtiles=Data[Data['TestBeamPE']>0.]
 #beamtiles=beamtiles.sort_values(by=['Bi207AveragePE'])
-x=beamtiles['Bi207AveragePE']
-print x
-xerr=0.05*x
-y=beamtiles['TestBeamPE']
+y=beamtiles['Bi207AveragePE']
 print y
-yerr=beamtiles['TestBeamPEError']
+yerr=0.05*y
+x=beamtiles['TestBeamPE']
+print x
+xerr=beamtiles['TestBeamPEError']
 names=beamtiles['TileName']
 print(names)
 
@@ -38,7 +38,7 @@ for i in range(0,len(names)):
 def func_pol1(x, a, b):
     return a*x+b
 
-fitres = optimize.curve_fit(func_pol1, x, y, sigma=yerr*10, p0=[4.,-16.], full_output=True, absolute_sigma=True)
+fitres = optimize.curve_fit(func_pol1, x, y, sigma=yerr, p0=[4.,-16.], full_output=True, absolute_sigma=True)
 popt=fitres[0]; pcov=fitres[1]
 # Need to use correlated errors to draw sigma bands
 a, b = unc.correlated_values(popt, pcov)
@@ -46,20 +46,21 @@ print('Fit results: \na = {0:.3f} \nb = {1:.3f}'.format(a,b))
 redchisq = (fitres[2]['fvec']**2).sum()/(len(fitres[2]['fvec'])-len(popt))
 print("chi2/Ndof = %6.3f" % redchisq)
 #print len(fitres[2]['fvec'])
-finex=np.arange(4.,16.,0.2)
-plt.ylim([0.,55.])
-plt.plot(finex, func_pol1(finex, *popt), 'b-',linewidth=2,label=r'Fit: $y={0:.2f}x {1:.2f}$'.format(*popt)) # fit line
+finex=np.arange(min(x),max(x),0.2)
+plt.ylim([0.,18.])
+plt.xlim([0.,55.])
+plt.plot(finex, func_pol1(finex, *popt), 'b-',linewidth=2,label=r'Fit: $y={0:.2f}x+{1:.2f}$'.format(*popt)) # fit line
 py = func_pol1(finex,a,b)
 nom = unp.nominal_values(py)
 std = unp.std_devs(py)
-plt.fill_between(finex, nom-1*std, nom+1*std, facecolor='b',alpha=0.3,label='1$\sigma$ band of fit') # bands
-plt.fill_between(finex, nom-2*std, nom+2*std, facecolor='b',alpha=0.2,label='2$\sigma$ band of fit') # bands
+#plt.fill_between(finex, nom-1*std, nom+1*std, facecolor='b',alpha=0.3,label='1$\sigma$ band of fit') # bands
+#plt.fill_between(finex, nom-2*std, nom+2*std, facecolor='b',alpha=0.2,label='2$\sigma$ band of fit') # bands
 
 plt.legend(loc='upper left', frameon=False, framealpha=1, numpoints=1)
 
 #plt.xlim(0.,10.)
 plt.title("")
-plt.xlabel(r"Bi207 Source [# PE]")
-plt.ylabel(r"Test Beam [# PE]")
+plt.ylabel(r"Bi207 Source [# PE]")
+plt.xlabel(r"Test Beam [# PE]")
 plt.grid()
 plt.savefig("BeamPE_vs_SourcePE.png")
